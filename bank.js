7b528475d2ab337dcdf58dbb27e924e0dc29b247
sys <sys@syss-MacBook-Pro.local>
2018-08-13 14:29:33 -0700

    Add CLI commands - deposit, withdraw, balance

--- bank.js
+++ bank.js
@@ -1,12 +1,42 @@
 var jsonStream = require('duplex-json-stream')
 var net = require('net')
 
+var log = []
+var commands = {
+    'deposit': function (sum, amount) {
+        return sum + amount
+    },
+    'withdraw': function (sum, amount) {
+        return sum - amount
+    },
+    'balance': function (sum, amount) {
+        return sum + amount
+    }
+}
+
 var server = net.createServer(function (socket) {
     socket = jsonStream(socket)
 
     socket.on('data', function (msg) {
         console.log('Bank received:', msg)
-        socket.write({ cmd: 'balance', balance: 0 })
+
+        var options = Object.keys(commands)
+        // console.log('OPTIONS: ' + options)
+        if (options.indexOf(msg.command) === -1) return
+        // console.log('INDEXOF: ' + options.indexOf(msg.command))
+        log.push(msg)
+        // console.log(log)
+        var balance = log.reduce(function (sum, msg) {
+            return commands[msg.command](sum, msg.amount)
+        }, 0)
+        if (balance < 0) {
+            log.pop()
+            socket.write('Invalid request: not enough funds.')
+            return
+        }
+        socket.write({
+            balance: balance
+        })
     })
 })
 
--- teller.js
+++ teller.js
@@ -1,10 +1,29 @@
 var jsonStream = require('duplex-json-stream')
 var net = require('net')
 
+// Build CLI - node teller.js deposit 123
+var cli = process.argv
+var command = cli[2]
+var amount = cli[3]
+
 var client = jsonStream(net.connect(3876))
 
-client.on('data', function (msg) {
+client.once('data', function (msg) {
     console.log('Teller received:', msg)
 })
 
-client.end({ cmd: 'balance' })
+var commands = ['deposit', 'withdraw', 'balance']
+if (commands.indexOf(command) === -1) {
+    console.error('Your command needs to be one of the followings.', JSON.stringify(commands))
+    client.end(null)
+}
+var msg = tellerRequest(command, parseFloat(amount, 10))
+client.end(msg)
+
+function tellerRequest(command, amount) {
+    var msg = {}
+    msg.command = command
+    msg.amount = amount || 0
+    console.log('Teller sent: ', msg)
+    return msg
+}
